a82b28e1bb18831f9fd11506286b5c14c7a76c7b
Rossana Suarez <46635001+roxsross@users.noreply.github.com>
2023-03-03 11:45:43 -0300

    init

--- automation/docker_build_push.sh
+++ automation/docker_build_push.sh
@@ -2,8 +2,8 @@
 
 VERSION=`jq -r '.version' ./package.json`
 REGION='us-east-1'
-ECS_ACCOUNT_NUMBER='459137896070'
-REPOSITORY='awsomedemo'
+ECS_ACCOUNT_NUMBER='000000000' #ACCOUNT_ID
+REPOSITORY='awsome-women'
 
 function ecr_login(){
   aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ECS_ACCOUNT_NUMBER.dkr.ecr.$REGION.amazonaws.com
